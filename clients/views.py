a2efa7f503c7d7f3add1c6e63d0933cf3d125cb9
Zic Stardust <git@zicstardust.com>
2023-12-21 01:14:11 -0400

    create insert clients and cars

--- clients/views.py
+++ clients/views.py
@@ -1,5 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Car, Client
+import re
 
 def clients(request):
-    return render (request, 'clients.html')
+    if request.method == "GET":
+        return render (request, 'clients.html')
+    elif request.method == "POST":
+        name = request.POST.get('name')
+        lastname = request.POST.get('lastname')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+
+        cars = request.POST.getlist('car')
+        plates = request.POST.getlist('plate')
+        years = request.POST.getlist('year')
+
+        client = Client.objects.filter(cpf=cpf)
+
+        if client.exists():
+            return render (request, 'clients.html', {'name': name, 'lastname': lastname, 'email': email, 'cars': zip(cars, plates, years)})
+
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
+            return render (request, 'clients.html', {'name': name, 'lastname': lastname, 'cpf': cpf, 'cars': zip(cars, plates, years)})
+
+        client = Client(
+            name = name,
+            lastname = lastname,
+            email = email,
+            cpf = cpf
+        )
+        client.save()
+
+        for car, plate, year in zip(cars, plates, years):
+            car = Car(
+                car = car,
+                plate = plate,
+                year = year,
+                client = client
+            )
+            car.save()
+
+        return HttpResponse("Sucessful Register")
--- templates/static/clients/js/clients.js
+++ templates/static/clients/js/clients.js
@@ -1,13 +1,11 @@
 function add_car(){
 
-    container = document.getElementById("form-car")
+    document.getElementById('form-car').innerHTML +=
+    "<br>\
+    <div class='row'> \
+    <div class='col-md'> <input type='text' placeholder='car' class='form-control' name='car'></div> \
+    <div class='col-md'><input type='text' placeholder='plate' class='form-control' name='plate'> </div> \
+    <div class='col-md'><input type='number' placeholder='year' class='form-control' name='year'> </div>\
+    </div>"
 
-    html = "<br><div class='row'> \
-    <div class='col-md'> <input type='text' placeholder='Car' class='form-control' name='car'></div> \
-    <div class='col-md'><input type='text' placeholder='Plate' class='form-control' name='plate'> </div>\
-    <div class='col-md'><input type='number' placeholder='Year' class='form-control' name='year'> </div>\
-    </div>";
-
-    container.innerHTML += html
-
-};
+}
