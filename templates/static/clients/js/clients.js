e25fdc613db943920f9f6392a983a79b7ea1c776
Zic Stardust <git@zicstardust.com>
2023-12-20 23:22:46 -0400

    add name

--- templates/static/clients/js/clients.js
+++ templates/static/clients/js/clients.js
@@ -3,9 +3,9 @@ function add_car(){
     container = document.getElementById("form-car")
 
     html = "<br><div class='row'> \
-    <div class='col-md'> <input type='text' placeholder='Car' class='form-control' name=''></div> \
-    <div class='col-md'><input type='text' placeholder='Plate' class='form-control'> </div>\
-    <div class='col-md'><input type='number' placeholder='Year' class='form-control'> </div>\
+    <div class='col-md'> <input type='text' placeholder='Car' class='form-control' name='car'></div> \
+    <div class='col-md'><input type='text' placeholder='Plate' class='form-control' name='plate'> </div>\
+    <div class='col-md'><input type='number' placeholder='Year' class='form-control' name='year'> </div>\
     </div>";
 
     container.innerHTML += html
